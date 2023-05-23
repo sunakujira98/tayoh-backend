984c4d7cd73f6215065ccfec8a779199bf98302e
Steven Theodore <steven@Stevens-MacBook-Pro.local>
2023-05-23 22:31:29 +0700

    Change user entity

--- model/entity/user.entity.go
+++ model/entity/user.entity.go
@@ -3,17 +3,16 @@ package entity
 import (
 	"time"
 
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID         uuid.UUID `db:"id" json:"id" validate:"required,uuid"`
-	Name	string	`json:"name"`
-	Email	string 	`json:"email"`
-	Address string `json:"address"`
-	Phone	string `json:"phone"`
-	CreatedAt time.Time	`json:"created_at"`
-	UpdatedAt time.Time	`json:"updated_at"`
+	ID        uint64         `db:"id" json:"id" gorm:"primaryKey;autoIncrement"`
+	Name      string         `json:"name"`
+	Email     string         `json:"email"`
+	Address   string         `json:"address"`
+	Phone     string         `json:"phone"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `json:"-" gorm:"index,column:deleted_at"`
 }
